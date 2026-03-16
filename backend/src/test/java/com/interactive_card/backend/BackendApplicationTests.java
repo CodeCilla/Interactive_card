652cff0f669e62d4cf234e095b6cbb0dbb61f03e
CodeCilla <priscilla.renault72460@gmail.com>
2026-03-16 13:42:30 +0100

    feat: setup docker orchestration for frontend, backend and database

---
+++ backend/src/test/java/com/interactive_card/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interactive_card.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
